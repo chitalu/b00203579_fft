7351d46e028a663a15c84bc5a81fc1797e3023b9
floyd_chitalu <Floyd.Chitalu@null.net>
2015-03-11 00:45:50 +0000

    add working example of forward and inverse fft. not fully tested yet

--- incl/base.h
+++ incl/base.h
@@ -2,5 +2,40 @@
 #define __BASE_H__
 
 
+struct cpu_timer_t {
+  typedef std::chrono::time_point<std::chrono::system_clock,
+                                  std::chrono::system_clock::duration>
+      cpu_prof_time_t;
+
+  cpu_timer_t(std::string prof_instance) : m_prof_instance(prof_instance) {
+    m_start_time = std::chrono::high_resolution_clock::now();
+  }
+
+  ~cpu_timer_t(void) {
+    m_end_time = std::chrono::high_resolution_clock::now();
+
+    profile_time_t elapsed =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(
+            m_end_time - m_start_time).count();
+    g_profiling_data[m_prof_instance].push_back(elapsed);
+
+    if (g_profiling_data[m_prof_instance].size() > g_profiling_samples) {
+      g_profiling_data[m_prof_instance].pop_front();
+    }
+  }
+
+private:
+  std::string m_prof_instance;
+  cpu_prof_time_t m_start_time, m_end_time;
+};
+
+#define PROFILE_()                                                    		   \
+  std::shared_ptr<cpu_timer_t> profiler_;                                 \
+  if (g_profiling_enabled) {                                                   \
+    std::shared_ptr<cpu_timer_t> p_inst =                                 \
+        std::shared_ptr<cpu_timer_t>(new cpu_timer_t(__FUNCTION__)); \
+    profiler_ = p_inst;                                                        \
+  }
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,56 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include <cstdio>
-//complex valued fft
+#include <cstdlib>
+#include <vector>
+
+// fft in the west!
 #include "fftw3.h"
 
-#define N (128)
+#define REAL_PART 0
+#define IMAGINARY_PART 1
 
-int main(int argc, char const *argv[])
-{
-	printf("%s\n", "hello fft!");
+#define Imag(x_) x_[IMAGINARY_PART]
+#define Real(x_) x_[REAL_PART]
 
-	/*fftw_complex in[N], out[N];
-    fftw_plan p;
-  
-    p = fftw_create_plan(N, FFTW_FORWARD, FFTW_ESTIMATE);
+#define N (1024)
 
-    fftw_one(p, in, out);
+const double two_pi = M_PI * 2.0;
+const int freq = 440;
+const double t = 0.0001;
 
-    fftw_destroy_plan(p);*/
+const double w = two_pi * freq;
 
-	return 0;
+int main(int argc, char const *argv[]) {
+  printf("%s\n", "hello fft!");
+
+  // time domain decomposed signal impulses
+  fftw_complex *x = (fftw_complex *)malloc(sizeof(fftw_complex) * N);
+  // frequency domain amplitudes
+  fftw_complex *X = (fftw_complex *)malloc(sizeof(fftw_complex) * N);
+  ;
+
+  for (int n = 0; n < N; ++n) {
+    Real(x[n]) = sin(w * (double)n * t);
+    Imag(x[n]) = 0;
+
+    Real(X[n]) = 0;
+    Imag(X[n]) = 0;
+  }
+
+  fftw_plan forward_fft_plan, inverse_fft_plan;
+
+  forward_fft_plan = fftw_plan_dft_1d(N, x, X, FFTW_FORWARD, FFTW_ESTIMATE);
+
+  fftw_execute(forward_fft_plan);
+
+  inverse_fft_plan = fftw_plan_dft_1d(N, X, x, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+  fftw_execute(forward_fft_plan);
+
+  fftw_destroy_plan(forward_fft_plan);
+  fftw_destroy_plan(inverse_fft_plan);
+
+  return 0;
 }
